834a761832f7802a5fbca9851e4d316cb561c988
Pradeep_Kumar_Rebbavarapu <rpkiit2022@gmail.com>
2023-05-17 17:46:04 +0530

    Added meta tag

--- cynaptics-website/src/app/layout.tsx
+++ cynaptics-website/src/app/layout.tsx
@@ -28,8 +28,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta property="og:image" content={`${process.env.SITE_URL}images/Logos/Logo.jpg`} />
+      <meta property="twitter:image" content={`${process.env.SITE_URL}/images/Logos/Logo.jpg`} />
+      <meta property="twitter:title" content="The Cynaptics Club - IIT Indore" />
+      <meta property="og:title" content={`${process.env.SITE_URL}/images/Logos/Logo.jpg`} />
+      <meta property="twitter:description" content="The Cynaptics Club(AI/ML) - IIT INDORE"/>
+      <meta property="og:description" content="The Cynaptics Club(AI/ML) - IIT INDORE" />
+        <meta property="og:image" content={`${process.env.SITE_URL}/images/Logos/Logo.jpg`} />
         <meta property="og:image:type" content="jpg" />
+        <meta property="og:url" content={process.env.SITE_URL} />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
       </head>
