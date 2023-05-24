8764082c25f4107f192d307520c874cfd7b840b3
Pradeep_Kumar_Rebbavarapu <rpkiit2022@gmail.com>
2023-05-24 23:25:32 +0530

    Few changed

--- cynaptics-website/src/app/AboutUsPage/page.jsx
+++ cynaptics-website/src/app/AboutUsPage/page.jsx
@@ -12,41 +12,7 @@ export default function AboutUspage() {
         About Us
       </h1>
       <h6 className="text-center mx-3 text-lg p-3 sm-text-sm">
-        Cynaptics Club is a dynamic community of students passionate about
-        harnessing the power of AI and ML to push the boundaries of innovation.
-        We believe that the future lies in the convergence of human intelligence
-        and advanced technologies, and our club serves as a hub for knowledge
-        sharing, skill development, and collaborative projects in this rapidly
-        evolving field. Through a diverse range of activities, we provide
-        valuable opportunities for our members to learn, experiment, and create.
-        Our club organizes engaging guest talks by industry professionals who
-        share their experiences, insights, and cutting-edge research, exposing
-        our members to the latest trends and developments in AI and ML. We also
-        host exhilarating hackathons, where you can put your skills to the test,
-        collaborate with peers, and build innovative solutions within a limited
-        timeframe. These hackathons foster teamwork, problem-solving, and
-        creativity, and they serve as an excellent platform for honing your AI
-        and ML skills. Additionally, we showcase our members' talents and
-        achievements through exhibitions, where innovative AI and ML projects
-        are displayed for the college community to see. These exhibitions not
-        only celebrate our members' hard work but also inspire others to delve
-        into the world of AI and ML. To cater to members at all levels of
-        expertise, we regularly organize workshops that cover various AI and ML
-        topics. From introductory sessions that lay the foundation of concepts
-        to advanced deep learning techniques, our workshops provide a
-        comprehensive learning experience that caters to your specific interests
-        and goals. We understand the importance of collaboration and collective
-        learning. Therefore, Cynaptics Club fosters an environment where members
-        can share their ideas, exchange knowledge, and engage in
-        thought-provoking conversations. These interactions facilitate a deeper
-        understanding of AI and ML concepts and encourage critical thinking.
-        Beyond technical learning, we actively bridge the gap between academia
-        and industry. We invite industry professionals to our events, providing
-        valuable networking opportunities and offering insights into career
-        paths and industry trends. We also encourage internships, industry
-        partnerships, and research opportunities to help our members gain
-        practical experience and prepare for successful careers in this rapidly
-        growing field.
+        {`Cynaptics Club is a dynamic community of students passionate about harnessing the power of AI and ML to push the boundaries of innovation. We believe that the future lies in the convergence of human intelligence and advanced technologies, and our club serves as a hub for knowledge sharing, skill development, and collaborative projects in this rapidly evolving field. Through a diverse range of activities, we provide valuable opportunities for our members to learn, experiment, and create. Our club organizes engaging guest talks by industry professionals who share their experiences, insights, and cutting-edge research, exposing our members to the latest trends and developments in AI and ML. We also host exhilarating hackathons, where you can put your skills to the test, collaborate with peers, and build innovative solutions within a limited timeframe. These hackathons foster teamwork, problem-solving, and creativity, and they serve as an excellent platform for honing your AI and ML skills. Additionally, we showcase our members' talents and achievements through exhibitions, where innovative AI and ML projects are displayed for the college community to see. These exhibitions not only celebrate our members' hard work but also inspire others to delve into the world of AI and ML. To cater to members at all levels of expertise, we regularly organize workshops that cover various AI and ML topics. From introductory sessions that lay the foundation of concepts to advanced deep learning techniques, our workshops provide a comprehensive learning experience that caters to your specific interests and goals. We understand the importance of collaboration and collective learning. Therefore, Cynaptics Club fosters an environment where members can share their ideas, exchange knowledge, and engage in thought-provoking conversations. These interactions facilitate a deeper understanding of AI and ML concepts and encourage critical thinking. Beyond technical learning, we actively bridge the gap between academia and industry. We invite industry professionals to our events, providing valuable networking opportunities and offering insights into career paths and industry trends. We also encourage internships, industry partnerships, and research opportunities to help our members gain practical experience and prepare for successful careers in this rapidly growing field.`}
       </h6>
       <h1 className="text-center py-3 text-3xl">Follow Our Socials</h1>
       <span className="flex justify-center pb-28">
