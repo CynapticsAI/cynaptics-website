d905c72fd5715c46a8b557c87fa1e5c9a676e239
Pradeep_Kumar_Rebbavarapu <rpkiit2022@gmail.com>
2023-05-26 13:59:06 +0530

    Removed index sitemap

--- cynaptics-website/next-sitemap.config.js
+++ cynaptics-website/next-sitemap.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     siteUrl: process.env.SITE_URL || 'https://cynaptics-website-ivory.vercel.app',
     generateRobotsTxt: true,
+    generateIndexSitemap: false
     
      // (optional)
     // ...other options
