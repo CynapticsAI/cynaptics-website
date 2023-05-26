82c92c6e407f1f0a4f1f7aeed93d36af046c681e
Pradeep_Kumar_Rebbavarapu <rpkiit2022@gmail.com>
2023-05-26 13:50:34 +0530

    Few Seo Changes

--- cynaptics-website/next-sitemap.config.js
+++ cynaptics-website/next-sitemap.config.js
@@ -1,5 +1,7 @@
 module.exports = {
     siteUrl: process.env.SITE_URL || 'https://cynaptics-website-ivory.vercel.app',
-    generateRobotsTxt: true, // (optional)
+    generateRobotsTxt: true,
+    
+     // (optional)
     // ...other options
   }
